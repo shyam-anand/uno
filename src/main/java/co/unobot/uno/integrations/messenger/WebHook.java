b791b5f8453c741a6f902f2e01d748c017f17bd3
Shyam Anand <shyamwdr@gmail.com>
2017-04-02 20:33:53 +0530

    messenger integration

--- src/main/java/co/unobot/uno/integrations/messenger/WebHook.java
+++ src/main/java/co/unobot/uno/integrations/messenger/WebHook.java
@@ -4,10 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -20,12 +17,17 @@ public class WebHook {
 
     private static final Logger logger = LoggerFactory.getLogger(WebHook.class);
 
-    @RequestMapping(method = {RequestMethod.POST, RequestMethod.GET})
+    @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity webHookPost(HttpServletRequest request,
                                       @RequestParam(value = "hub.mode") String hubMode,
                                       @RequestParam(value = "hub.challenge") String hubChallenge) {
         logger.info(request.getMethod() + " - " + request.getQueryString());
-
         return new ResponseEntity<>(hubChallenge, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity webHook(@RequestBody String requestBody) {
+        logger.info("POST - " + requestBody);
+        return new ResponseEntity<>(requestBody, HttpStatus.OK);
     }
 }
