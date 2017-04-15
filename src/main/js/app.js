620579dc6a0b7b4413a9af4e1fecef60b8a54a10
Shyam Anand <shyamwdr@gmail.com>
2017-04-15 12:13:02 +0530

    frontend

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.FB = props.FB;
+        this.FB = props.fb;
 
         this.state = {
             fbLoginStatus: false,
