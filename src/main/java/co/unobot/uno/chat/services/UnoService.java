cee2d249e644bd92cf30b1511bcc742296161278
Shyam Anand <shyamwdr@gmail.com>
2017-04-20 16:05:52 +0530

    agents bulk add

--- src/main/java/co/unobot/uno/chat/services/UnoService.java
+++ src/main/java/co/unobot/uno/chat/services/UnoService.java
@@ -108,9 +108,6 @@ public class UnoService {
             );
         }
 
-        unoResponse.getParameters().forEach((key, value) -> {
-            logger.info(key + " -> " + value.toString());
-        });
         return unoResponse;
     }
 
